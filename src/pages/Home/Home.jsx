f2c6b76e5c5237ea15675a5895bfc2efbad35169
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-03 07:12:58 +0600

    banner second part working

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 // import Navbar from "../../components/shareComponents/Navbar";
 
+import Banner from "./Banner";
+
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
           home...
         </div>
     );
