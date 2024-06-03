d8234250cd007a5ceaf5b6daf18ca4b6eb99e9ae
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-03 11:58:29 +0600

    added authProvider

--- src/components/shareComponents/Navbar.jsx
+++ src/components/shareComponents/Navbar.jsx
@@ -20,7 +20,7 @@ const navLinks=<>
                            }
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">Educational <span className="text-blue-600">Assistance</span></a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
