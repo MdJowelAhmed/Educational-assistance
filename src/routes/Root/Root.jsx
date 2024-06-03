e01ab98ea2b70eaadd18266083270cb1b877389e
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-03 10:36:39 +0600

    added banner section

--- src/routes/Root/Root.jsx
+++ src/routes/Root/Root.jsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/shareComponents/Navbar";
+import Footer from "../../components/shareComponents/Footer";
 
 
 const Root = () => {
     return (
         <div>
-            <div>
+            <div className="mx-16 mt-10">
                 <Navbar></Navbar>
             </div>
-           <Outlet></Outlet>
+            <div className="mx-16 my-16 min-h-80">
+                <Outlet></Outlet>
+            </div>
+            <div className="mx-16 mb-16">
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
