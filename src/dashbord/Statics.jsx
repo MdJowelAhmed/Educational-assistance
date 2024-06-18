4eddae1adcef2575e4621351dc203ed508fbf3f4
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-06-18 06:53:54 +0600

    SPINNER Add

--- src/dashbord/Statics.jsx
+++ src/dashbord/Statics.jsx
@@ -4,7 +4,7 @@ import useAuth from "../Hooks/useAuth";
 const Statics = () => {
     const {user}=useAuth()
     return (
-        <div className='bg-base-200 h-full p-16 rounded-xl'>
+        <div className='bg-base-200 h-full p-16 rounded-xl '>
             <h2 className="text-xl font-bold ">Hi,<span className="text-4xl font-bold text-green-600"> {user?.displayName}</span> welcome To your Dashboard..... <br />Check Your activities step by step</h2>
         </div>
     );
--- src/dashbord/moderator/moderatorProfile/ModeratorProfile.jsx
+++ src/dashbord/moderator/moderatorProfile/ModeratorProfile.jsx
@@ -1,12 +1,17 @@
 import useAuth from "../../../Hooks/useAuth";
+import useRole from "../../../Hooks/useRole";
+import LoadingSpinner from "../../../pages/Spinner/Spinner";
 
 
 const ModeratorProfile = () => {
-    const {user}=useAuth()
+    const {user,loading}=useAuth()
+    const [role, isLoading] = useRole()
+    if (isLoading || loading) return <LoadingSpinner />
     return (
         <div className="flex flex-col justify-center items-center h-96 bg-base-200">
            <img className="w-32 h-32 rounded-full" src={user?.photoURL} alt="" />
            <h2 className="my-7 text-4xl font-lato">{user?.displayName}</h2>
+           <h2 className="my-7 text-2xl font-lato">{user?.email}</h2>
         </div>
     );
 };
